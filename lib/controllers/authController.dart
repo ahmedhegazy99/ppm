4c3b4c412c0b086591f588de89ba6ee258cd0b74
JabalSoftDev35 <dev35@jabalsoft.com>
2021-08-20 04:12:37 +0200

    fix auth errors

--- lib/controllers/authController.dart
+++ lib/controllers/authController.dart
@@ -11,7 +11,7 @@ class AuthController extends GetxController {
   FirebaseAuth _auth = FirebaseAuth.instance;
   var _user = Rxn<User>();
   var loading = false.obs;
-
+  var userType = UserTypeEnum.userPlayer.obs;
   User? get user => _user.value;
 
   @override
@@ -23,9 +23,9 @@ class AuthController extends GetxController {
             await Get.find<DatabaseController>().getUser(_.uid);
       }
     });
-    ever(loading, (val) {
+    ever(loading, (bool? val) {
       //!= null is add by me not sure of it
-      if (val != null)
+      if (val != null && val)
         Get.defaultDialog(
             title: 'loading'.tr, content: CircularProgressIndicator());
       else
@@ -37,13 +37,13 @@ class AuthController extends GetxController {
       String mobile, String password) async {
     try {
       loading.toggle();
-      UserCredential _authResult =
-     /* AuthResult _authResult =*/ await _auth.createUserWithEmailAndPassword(
-          email: email.trim(), password: password);
+      UserCredential _authResult = /* AuthResult _authResult =*/ await _auth
+          .createUserWithEmailAndPassword(
+              email: email.trim(), password: password);
       UserModel _user = UserModel(
           id: _authResult.user!.uid,
           name: name,
-          userType: userType,
+          userType: userType.value,
           mobile: mobile,
           email: email);
       await Get.find<DatabaseController>().createNewUser(_user);
@@ -60,8 +60,8 @@ class AuthController extends GetxController {
     try {
       loading.toggle();
       await _auth.signInWithEmailAndPassword(email: email, password: password);
-      //Get.offAllNamed(AppRouter.mainBarRoute);
-      Get.offAllNamed(AppRouter.contactRoute);
+      Get.offAllNamed(AppRouter.mainBarRoute);
+      // Get.offAllNamed(AppRouter.contactRoute);
       print("start");
       loading.toggle();
       print("finish");
@@ -83,11 +83,4 @@ class AuthController extends GetxController {
       displayError(e);
     }
   }
-  var userType = UserTypeEnum.userPlayer.obs as UserTypeEnum;
-  void onClickRadioButton(value) {
-    print(value);
-    userType = value;
-    update();
-  }
-
 }
--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -4,29 +4,26 @@ part 'userModel.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class UserModel {
-  String ?id;
-  UserTypeEnum ? userType;
+  String? id;
+  UserTypeEnum? userType;
   //Rx<UserTypeEnum> ? userType;
-  String ?name;
-  String ?email;
-  String ?mobile;
-  String ?imageUrl;
+  String? name;
+  String? email;
+  String? mobile;
+  String? imageUrl;
 
   UserModel(
-      { this.id,
-        this.userType,
-        this.name,
-        this.mobile,
-        this.imageUrl,
-        this.email
-      });
+      {this.id,
+      this.userType,
+      this.name,
+      this.mobile,
+      this.imageUrl,
+      this.email});
 
   UserModel copy() => UserModel.fromJson(this.toJson());
-  factory UserModel.fromJson(Map<String, dynamic> json) =>
-      _$UserModelFromJson(json);
+  factory UserModel.fromJson(Map<String, dynamic>? json) =>
+      _$UserModelFromJson(json!);
   Map<String, dynamic> toJson() => _$UserModelToJson(this);
-
 }
 
 enum UserTypeEnum { admin, club, userPlayer }
-
